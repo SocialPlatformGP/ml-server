6eaf2ab3184cce38ba4383ae02fd492b6a54724a
Edrees <mohamededrees234@hotmail.com>
2024-05-03 14:10:23 +0300

    initialized repo

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from transformers import pipeline
+import requests
+from PIL import Image
+from urllib.parse import unquote
+# API_URL = "https://api-inference.huggingface.co/models/giacomoarienti/nsfw-classifier"
+# headers = {"Authorization": "Bearer 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"}
+app = FastAPI()
+nlp = pipeline("sentiment-analysis", model='akhooli/xlm-r-large-arabic-toxic')
+classifier = pipeline("image-classification", model="AdamCodd/vit-base-nsfw-detector")
+@app.get('/')
+async def root():
+    return {'example': 'This is an example', 'data': 10}
+
+@app.get('/validate-text/')
+async def validate_text(text: str):
+    result = nlp(text)
+    return {'data': text, 'result': result}
+
+@app.get('/validate-image')
+async def validate_image(url: str):
+    image = Image.open(requests.get(url, stream=True).raw)
+    result = classifier(image)
+    return {'url':url, 'result': result}
